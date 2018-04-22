d01f09f40a11b4b7b0255264fb5f36947de8e411
Juan Nunez <juan.nunez1193@gmail.com>
2018-04-21 22:40:33 -0400

    example

--- example/main.go
+++ example/main.go
@@ -3,7 +3,6 @@ package main
 import "go-identicon"
 
 func main() {
-	//icon := identicon.NewIdenticon("Hello world, how are you")
-	icon := identicon.NewIdenticon("Wasd wasd yoo")
+	icon := identicon.NewIdenticon("Hello world, how are you")
 	icon.ToImage()
 }
